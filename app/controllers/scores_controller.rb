68685c08be2a3e1b9341ad37afed1352b7b37f44
Lyoung0789 <Lestermyoung07@gmail.com>
2020-10-08 18:02:51 -0400

    adding create function in the scores controller to handle a new submission to the DB

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -5,5 +5,20 @@ class ScoresController < ApplicationController
         render json: @scores
     end 
 
+    def create
+        binding.pry
+        score = Score.new(score_params)
+        if goal.save
+            render json: score
+        else 
+            render :json => {:error => score.errors}
+        end 
+    end 
+
+    private
+
+    def score_params
+        params.require(:score).permit(:name, :score)
+    end 
     
 end
